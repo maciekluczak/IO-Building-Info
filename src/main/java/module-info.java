7f1c481bae7e6d26bf89e0e5218abb3c7397087f
Vasya Pupkin <mineralsfree@gmail.com>
2021-12-19 03:24:06 +0100

    javafx inital commit

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -8,6 +8,7 @@ module pl.put.poznan.transformer.gui.app {
 
 
     opens pl.put.poznan.transformer.gui.app to javafx.fxml;
+    opens pl.put.poznan.transformer.models to javafx.base;
     exports pl.put.poznan.transformer.gui.app;
     exports pl.put.poznan.transformer.app;
 }
--- src/main/java/pl/put/poznan/transformer/gui/app/HelloApplication.java
+++ src/main/java/pl/put/poznan/transformer/gui/app/HelloApplication.java
@@ -13,7 +13,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
 
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
+        Scene scene = new Scene(fxmlLoader.load(), 800, 600);
         stage.setTitle("Building info");
         stage.setScene(scene);
         stage.show();
