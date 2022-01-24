3862c89c2f21d70268a38576ce269c882e48d7f9
PePeX21 <67105405+PePeX21@users.noreply.github.com>
2022-01-24 21:40:02 +0100

    na dole znajduje sie funkcja

--- src/main/java/pl/put/poznan/transformer/models/Room.java
+++ src/main/java/pl/put/poznan/transformer/models/Room.java
@@ -10,30 +10,34 @@ import java.util.List;
 public class Room extends Location {
     private float area, cube, heating, light;
 
+
+
+
     /**
      * konstruktor z parametrami:
      *
-     * @param name      nazwa pokoju
-     * @param area      powierzchnia pokoju
-     * @param cube      kubatura pokoju
-     * @param heating   poziom zuzycia energii ogrzewania
-     * @param light     laczna moc oswietlenia w pokoju
+     * @param name    nazwa pokoju
+     * @param area    powierzchnia pokoju
+     * @param cube    kubatura pokoju
+     * @param heating poziom zuzycia energii ogrzewania
+     * @param light   laczna moc oswietlenia w pokoju
      */
 
-    public Room( String name, float area, float cube, float heating, float light){
-        super( name);
+    public Room(String name, float area, float cube, float heating, float light) {
+        super(name);
         this.area = area;
         this.cube = cube;
         this.heating = heating;
         this.light = light;
     }
 
+
     /**
      * funkcja zwracajaca powierzchnie pokoju
      *
      * @return powierzchnia pokoju
      */
-    public float getArea(){
+    public float getArea() {
         return area;
     }
 
@@ -42,7 +46,7 @@ public class Room extends Location {
      *
      * @return kubatura pokoju
      */
-    public float getCube(){
+    public float getCube() {
         return cube;
     }
 
@@ -51,7 +55,7 @@ public class Room extends Location {
      *
      * @return energia na ogrzewanie w pokoju
      */
-    public float getHeating(){
+    public float getHeating() {
         return heating;
     }
 
@@ -60,7 +64,7 @@ public class Room extends Location {
      *
      * @return moc oswietlenia
      */
-    public float getLight(){
+    public float getLight() {
         return light;
     }
 
@@ -69,8 +73,8 @@ public class Room extends Location {
      *
      * @return srednie zuzycie energii na m2
      */
-    public  float getHeatingPerUnit(){
-        return heating/area;
+    public float getHeatingPerUnit() {
+        return heating / area;
     }
 
     /**
@@ -78,23 +82,132 @@ public class Room extends Location {
      *
      * @return srednia moc oswietlenia na m3
      */
-    public  float getLightPerUnit(){
-        return light/cube;
+    public float getLightPerUnit() {
+        return light / cube;
     }
 
     /**
      * funkcja sprawdzajaca czy pokoj przekracza limit wykorzystania energii na ogrzewanie
      *
-     * @param limit     wartosc limitu dopuszczanego zuzycia energii
+     * @param limit wartosc limitu dopuszczanego zuzycia energii
      * @return lista pokojow przekraczajacych limit zuzycia
      */
-    public List<Room> getHeatingOverLimit(float limit){
+    public List<Room> getHeatingOverLimit(float limit) {
         List<Room> roomsList = new ArrayList<Room>();
         float heatingPU = this.heating / this.cube;
-        if (heatingPU > limit){
+        if (heatingPU > limit) {
             roomsList.add(this);
         }
         return roomsList;
     }
 
+    private boolean isCorrect(float value) {
+        if (value < 0) {
+            throw new IllegalArgumentException("Incorrect input values");
+        }
+        return false;
+        //float maxValue = validValues.getMaxValue();
+        //float minValue = validValues.getMinValue();
+        //if (value > minValue && value < maxValue) {
+        //    return true;
+        //}
+        //return false;
+    }
 }
+
+
+
+
+
+/*
+/**
+     * konstruktor z parametrami:
+     *
+     * @param name      nazwa pokoju
+     * @param area      powierzchnia pokoju
+     * @param cube      kubatura pokoju
+     * @param heating   poziom zuzycia energii ogrzewania
+     * @param light     laczna moc oswietlenia w pokoju
+     */
+/*
+
+     /**
+     * pusty konstruktor
+     */
+/*
+    Public Room(){
+    }
+
+    public Room( String name, float area, float cube, float heating, float light){
+        super( name);
+        if(name == null){
+            throw new IllegalArgumentException("input name can not be null");
+        }
+        //boolean isAreaCorrect = isCorrect(area, ParamValues.AREA);
+        //boolean isCubeCorrect = isCorrect(cube, ParamValues.CUBE);
+        //boolean isHeatingCorrect = isCorrect(heating, ParamValues.HEATING);
+        //boolean isLightCorrect = isCorrect(light, ParamValues.LIGHT);
+        //if (isAreaCorrect && isCubeCorrect && isHeatingCorrect && isLightCorrect){
+        this.area = area;
+        this.cube = cube;
+        this.heating = heating;
+        this.light = light;
+        //}
+        //else{
+        //    throw new IllegalArgumentException("Incorrect input values");
+        //}
+
+
+
+    }
+
+
+    /**
+     * funkcja  sluzacy do oceny poprawnosci danych wejsciowych
+     * @param name
+     * @param area
+     * @param cube
+     * @param heating
+     * @param light
+     * @return true/false
+     */
+/*
+    public boolean testingRoomsInput(String name, float area, float cube, float heating, float light){
+
+        if(name == null){
+            throw new IllegalArgumentException("input name can not be null");
+        }
+
+        boolean isAreaCorrect = isCorrect(area, ParamValues.AREA);
+        boolean isCubeCorrect = isCorrect(cube, ParamValues.CUBE);
+        boolean isHeatingCorrect = isCorrect(heating, ParamValues.HEATING);
+        boolean isLightCorrect = isCorrect(light, ParamValues.LIGHT);
+        if (isAreaCorrect && isCubeCorrect && isHeatingCorrect && isLightCorrect){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+
+
+    /**
+     * funkcja pomocnicza sluzacy do oceny poprawnosci danych wejsciowych
+     * @param value
+     * @param validValues
+     * @return prawda/falsz
+     */
+/*
+    private boolean isCorrect(float value, ParamValues validValues){
+        if(value < 0) {
+            throw new IllegalArgumentException("Incorrect input values");
+        }
+
+        float maxValue = validValues.getMaxValue();
+        float minValue = validValues.getMinValue();
+        if (value > minValue && value < maxValue) {
+            return true;
+        }
+        return false;
+    }
+ */
